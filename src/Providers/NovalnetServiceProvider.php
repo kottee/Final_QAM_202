81217155eb9d9a162a358bfed7576d373af29e9e
kotteeswaran <kotteeswaranviit@gmail.com>
2019-03-10 20:54:32 +0530

    Update NovalnetServiceProvider.php

--- src/Providers/NovalnetServiceProvider.php
+++ src/Providers/NovalnetServiceProvider.php
@@ -211,7 +211,7 @@ class NovalnetServiceProvider extends ServiceProvider
 						$guaranteeStatus = $paymentService->getGuaranteeStatus($basketRepository->load(), $paymentKey);
 			    
 				
-			    //$this->getLogger(__METHOD__)->error('2', 'test');	
+
 			    $redirect = $paymentService->isRedirectPayment($paymentKey);		
 			    if ($redirect && $paymentKey != 'NOVALNET_CC') { # Redirection payments
 							$serverRequestData = $paymentService->getRequestParameters($basketRepository->load(), $paymentKey);
